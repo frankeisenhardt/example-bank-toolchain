a67e41cf9000a11b5bd3adfe5da6e52f1c9a17d7
frankeisenhardt <frank.eisenhardt@de.ibm.com>
2021-08-10 11:06:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*eslint-env node*/
 
 //------------------------------------------------------------------------------
-// node.js starter application for Bluemix Branch2
+// node.js starter application for Bluemix Branch254
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
